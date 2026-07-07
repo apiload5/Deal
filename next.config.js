42b04b4cc227269e1be737878139b76ae22752fb
apiload5 <amir03182055632@gmail.com>
2026-07-07 07:01:32 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -32,9 +32,7 @@ const nextConfig = {
     unoptimized: process.env.NODE_ENV === 'development',
   },
   experimental: {
-    serverActions: true,
-    typedRoutes: true,
-    allowRequestExtensionHooks: true,
+    typedRoutes: true
   },
   env: {
     NEXT_PUBLIC_CURRENCY: 'Rs',
